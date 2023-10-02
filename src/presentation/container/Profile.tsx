2cd5da96daf20a5802b97a677287cab6d7eb6861
LILLINH <tunglinhwork@gmail.com>
2023-10-02 21:02:23 +0700

    quenmatkhau

---
+++ src/presentation/container/Profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const Profile = () => {
+  return (
+    <View>
+        
+    </View>
+  );
+};
+export default Profile;
+const styles = StyleSheet.create({
+});
