73917fb8d2e41ab7923b47ac540187de697d3082
LILLINH <tunglinhwork@gmail.com>
2023-10-03 14:39:37 +0700

    deals

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Test from './src/presentation/container/Test';
 import ForgotPass from './src/presentation/container/ForgotPass';
 import Profile from './src/presentation/container/Profile';
+import Deals from './src/presentation/container/Deals';
 function App(): JSX.Element {
   return (
-    <Profile/>
+    <Deals/>
   );
 }
 
