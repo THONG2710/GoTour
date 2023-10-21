a3616968b2a0e3b199216e986cd44ccf71e0b6cc
huyho <huyho17032003@gmail.com>
2023-10-21 19:28:12 +0700

    huy

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Test from './src/presentation/container/Test';
+import Train from './src/presentation/container/screens/Train';
+import Plane from './src/presentation/container/screens/Plane';
+import Hotel from './src/presentation/container/screens/Hotel';
+
+
+
 function App(): JSX.Element {
   return (
-    <Test/>
+      // <Train/>
+      // <Test/>
+      // <Plane/>
+      <Hotel/>
   );
 }
 
